d9ba1771cf34736f1a563ea877adb1171e91a0f8
erdemcobanoglu <erdemcobanoglu@users.noreply.github.com>
2019-04-04 20:09:18 +0300

    sorgulara başlandı getall sorgularımız

---
+++ EntitiesLayer/SqlQuery/QueryWarehouse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer.SqlQuery
+{
+    public static class QueryWarehouse
+    {
+        public static class Account
+        {
+            public static string Insert => "";
+            public static string Update => "";
+            public static string Delete => "";
+            public static string GetAll => "select * from Account with(nolock)";
+            public static string GetById => "";
+        }
+        public static class City
+        {
+            public static string Insert => "";
+            public static string Update => "";
+            public static string Delete => "";
+            public static string GetAll => "select * from City with(nolock)";
+            public static string GetById => "";
+        }
+        public static class Country
+        {
+            public static string Insert => "";
+            public static string Update => "";
+            public static string Delete => "";
+            public static string GetAll => "select * from Country with(nolock)";
+            public static string GetById => "";
+        } 
+        public static class Customer
+        {
+            public static string Insert => "";
+            public static string Update => "";
+            public static string Delete => "";
+            public static string GetAll => "select * from Customer with(nolock)";
+            public static string GetById => "";
+        }
+    }
+}
